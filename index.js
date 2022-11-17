61dc128334058f5bb7d58eb38dd029104ef1be67
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-17 16:25:41 +0600

    get appointment

--- index.js
+++ index.js
@@ -12,9 +12,37 @@ app.use(express.json());
 //mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wgk9s.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
+async function run (){
+try{
+  const appointmentCollection = client.db('doctors_portal').collection('appointmentOptions');
+
+  //get appointment
+  // app.get('/appointment', async (req, res) => {
+  //   const query = {}
+  //   const cursor = appointmentCollection.find(query);
+  //   const appointments = await cursor.toArray();
+  //   res.send(appointments);
+  // });
+  
+  app.get('/appointment',async(req,res)=>{
+    const query = {};
+    const cursor = appointmentCollection.find(query);
+    const appointments = await cursor.toArray();
+    res.send(appointments);
+  })
+
+}
+
+ finally{
+
+  }
+}
+
+run().catch(err => console.err(err));
 
 app.get('/', (req, res) => {
   res.send('Hello from doctors portal')
